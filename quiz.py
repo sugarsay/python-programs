5e02ecfb8f16682b5636b0f0d12f17a44708c812
Leja Podobnik <leja123@users.noreply.github.com>
2021-10-04 16:47:24 +0200

    added quiz.py

---
+++ quiz.py
@@ -0,0 +1,9 @@
+print ("Hello there, welcome to the quiz!")
+print ("Answer the following question with yes or no")
+answer = input("5 is greater than 1: ")
+answer1 = answer
+if answer1 == "yes":
+    print("Your answer is correct!")
+else: 
+    print("Your answer was not correct")
+
