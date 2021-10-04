959d4e15c248c41be1808ef7649c765f4cde785d
Leja Podobnik <leja123@users.noreply.github.com>
2021-10-04 17:27:44 +0200

    added addition program

---
+++ addition.py
@@ -0,0 +1,8 @@
+number1 = input("Enter 1st number: ")
+number1 = float(number1)
+
+number2 = input("Enter 2nd number: ")
+number2 = float(number2)
+
+result = number1 + number2
+print("Result is: ", result)
