99409ea981ee035a4cb307f0bd86047be32b3d97
Leja Podobnik <leja123@users.noreply.github.com>
2022-10-03 21:51:53 +0200

    added calendar

---
+++ calendar.py
@@ -0,0 +1,49 @@
+import calendar
+#january   
+yy = 2022
+mm = 1
+print(calendar.month(yy, mm))
+#february 
+yy = 2022
+mm = 2
+print(calendar.month(yy, mm))
+#march  
+yy = 2022
+mm = 3
+print(calendar.month(yy, mm))
+#april   
+yy = 2022
+mm = 4
+print(calendar.month(yy, mm))
+#may   
+yy = 2022
+mm = 5
+print(calendar.month(yy, mm))
+#june  
+yy = 2022
+mm = 6
+print(calendar.month(yy, mm))
+#july   
+yy = 2022
+mm = 7
+print(calendar.month(yy, mm))
+#august
+yy = 2022
+mm = 8
+print(calendar.month(yy, mm))
+#september  
+yy = 2022
+mm = 9
+print(calendar.month(yy, mm))
+#october  
+yy = 2022
+mm = 10
+print(calendar.month(yy, mm))
+#november 
+yy = 2022
+mm = 11
+print(calendar.month(yy, mm))
+#december
+yy = 2022
+mm = 12
+print(calendar.month(yy, mm))
